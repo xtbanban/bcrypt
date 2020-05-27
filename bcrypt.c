f167bf406c5ddf5d9d79bd76587f0affe07ec6b8
xtbanban <88185785@qq.com>
2020-05-28 00:34:38 +0800

    完善参数输入。

--- bcrypt.c
+++ bcrypt.c
@@ -207,11 +207,12 @@ int checkpw(unsigned char* input, char* bcrypt_output) {
 char* bcrypt_output_sure(unsigned char* input, unsigned int rounds, unsigned int rand_num, unsigned int times) {
 	char* output;
 	if (times < 3) times = 3;
+	unsigned int i = times;
 again:
-	output = bcrypt_output(input, rounds, rand_num + times * 10);
+	output = bcrypt_output(input, rounds, rand_num + i * 10);
 	if (!checkpw(input, output)) {
-		times--;
-		if (times) {
+		i--;
+		if (i) {
 			goto again;
 		} else {
 			goto next;
@@ -219,49 +220,81 @@ again:
 	}	
 next:
 	if (debug)
-		printf("times = %d\n", times);
+		printf("trys = %d / %d\n", (times - i + 1), times);
 
 	return output;	
 }	
 
 int main(int argc, char* argv[]) {	
 	int k = 0, count;
-	char* input = "123456";
-	
-	count = 1;
-	if (argc > 1) {
-	count = atoi(argv[1]);
-		if (count <= 0)
-			count = 1;
-	}		
+	char* input;
 	
 	debug = 0;
-	if (argc > 2) {
-		if (strcmp(argv[2], "debug") == 0) 
-			debug = 1;
-	}	
 	
 	if (argc == 1) {
+		printf("%s [password] [times] [debug]\n", argv[0]);
+		printf(" Get the bcrypt_output text(60bit).\n");
+		printf("-text:password: bcrypt passwordw;\n");
+		printf("-int:times: Output more result;\n");
+		printf("-debug: Show debug messages.\n");
+		printf("------------------------------;\n");
+		printf("%s [password] [bcrypt_output]\n", argv[0]);
+		printf(" Check the bcrypt_output text(60bit).\n");
+		printf("-text:password: bcrypt passwordw;\n");
+		printf("-text:bcrypt_output(60bit).\n");
+		
+	} else if (argc == 2) {
+		input = argv[1];
 		char* output = bcrypt_output_sure(input, 10, 0, 10);
 		printf("%s\n", output);
-	} else {
-		for (int j = 0; j < count; j++) {	
-			printf("-------|--------------------||-----------------------------|-------\n");
-			char* output = bcrypt_output_sure(input, 10, j, 10);
-			printf("%s\n", output);
 		
-			if (checkpw(input, output)) {
-				k++;
-				printf("Check OK.\n");
+	} else {	
+		input = argv[1];
+		
+		char* check = argv[2];
+		if (strlen(check) == 60) {
+			if (checkpw(input, check)) {
+				printf("Check OK.\n");	
 			} else {
 				printf("Check Erro!\n");
-			}
+			}			
+			return 0;
+		}	
 		
-			free(output);
-		}  
+		count = atoi(argv[2]);
+		if (count <= 0)
+			count = 1;
+
+		if (argc > 3) {
+			if (strcmp(argv[3], "debug") == 0) 
+				debug = 1;
+		}
+
+		if (debug) {
+			for (int j = 0; j < count; j++) {	
+				printf("-------|--------------------||-----------------------------|-------\n");
+				char* output = bcrypt_output_sure(input, 10, j, 10);
+				printf("%s\n", output);
+		
+				if (checkpw(input, output)) {
+					k++;
+					printf("Check OK.\n");
+				} else {
+					printf("Check Erro!\n");
+				}
+		
+				free(output);
+			}  
 	
-		printf("-------------------------------------------------------------------\n");
-		printf("Check OK SUM = %d\n", k);
+			printf("-------------------------------------------------------------------\n");
+			printf("Check OK SUM = %d\n", k);
+			
+		} else {
+			for (int j = 0; j < count; j++) {	
+				char* output = bcrypt_output_sure(input, 10, j, 10);
+				printf("%s\n", output);		
+			}
+		}	
 	}
 	
 	//system("pause");
