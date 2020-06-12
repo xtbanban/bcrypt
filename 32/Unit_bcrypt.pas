cd79a5ed056417d7bd868ea9181b9943ae3da881
xtbanban <88185785@qq.com>
2020-06-12 22:16:15 +0800

    delphi7

--- 32/Unit_bcrypt.pas
+++ 32/Unit_bcrypt.pas
@@ -18,8 +18,41 @@ type
     Bevel1: TBevel;
     Label3: TLabel;
     Edit_rounds: TEdit;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Edit_key_1: TEdit;
+    Edit_output_1: TEdit;
+    BitBtn_outpuy_1: TBitBtn;
+    BitBtn_check_1: TBitBtn;
+    Edit3: TEdit;
+    Bevel2: TBevel;
+    Bevel3: TBevel;
+    Label_note_1: TLabel;
+    Bevel4: TBevel;
+    Label7: TLabel;
+    Bevel5: TBevel;
+    Bevel6: TBevel;
+    Bevel7: TBevel;
+    Bevel8: TBevel;
+    Bevel9: TBevel;
+    Bevel10: TBevel;
+    Bevel11: TBevel;
+    Bevel12: TBevel;
+    SpeedButton_up: TSpeedButton;
+    Edit_salt: TEdit;
+    Label8: TLabel;
+    Bevel13: TBevel;
+    Edit_org_1: TEdit;
+    Label9: TLabel;
+    Label10: TLabel;
+    Label11: TLabel;
+    Label12: TLabel;
     procedure BitBtn_bcryptClick(Sender: TObject);
     procedure BitBtn_checkClick(Sender: TObject);
+    procedure BitBtn_outpuy_1Click(Sender: TObject);
+    procedure BitBtn_check_1Click(Sender: TObject);
+    procedure SpeedButton_upClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -31,19 +64,21 @@ var
 
 implementation
 
+uses
+  BCrypt;
+
 {$R *.dfm}
 
 function checkpw(
-      input: PChar;
-      bcrypt_output: PChar): Integer;
-    stdcall; external 'bcrypt32.dll';
+  input: PChar;
+  bcrypt_output: PChar): Integer;
+  stdcall; external 'bcrypt32.dll';
+
 function bcrypt_output_sure(
-      input: PChar;
-      rounds: integer;
-      times: integer;
-      rand_num: integer;
-      debug: boolean): PChar;
-    stdcall; external 'bcrypt32.dll';
+  input: PChar;
+  rounds: integer;
+  times: integer): PChar;
+  stdcall; external 'bcrypt32.dll';
 
 procedure TForm1.BitBtn_bcryptClick(Sender: TObject);
 var
@@ -62,17 +97,25 @@ begin
   Edit_rounds.Text := IntToStr(I);
   Edit_output.Text := '';
   Edit_output.Refresh;
+  Label_tip.Caption := 'Bcrypt...';
+  Label_tip.Refresh;
   p1 := pchar(S);
-  p2 := bcrypt_output_sure(p1, I, 10, 1, false);
+  p2 := bcrypt_output_sure(p1, I, 10);
   Edit_output.Text := p2;
-  Label_tip.Caption := '';
+  Label_tip.Caption := 'Bcrypt finish.';
 end;
 
 procedure TForm1.BitBtn_checkClick(Sender: TObject);
 var
   p1, p2: PChar;
+  S: string;
 begin
-  p1 := pchar(Trim(Edit_key.Text));
+  S := Trim(Edit_key.Text);
+  if (S = '') then
+  begin
+    Label_tip.Caption := 'Pleas input key.';
+    Exit;
+  end;  p1 := pchar(Trim(Edit_key.Text));
   p2 := pchar(Trim(Edit_output.Text));
   if (checkpw(p1, p2) = 1) then
   begin
@@ -83,4 +126,44 @@ begin
   end;
 end;
 
+procedure TForm1.BitBtn_outpuy_1Click(Sender: TObject);
+var
+  key, org, salt, output: ansistring;
+  keyBtypts, saltBytes: TBytes;
+begin
+  key := Trim(Edit_key_1.Text);
+  org := Trim(Edit_org_1.Text);
+  if (Length(org) < 16) then
+  begin
+    Label_note_1.Caption := 'org length must move then 16';
+    exit;
+  end;
+  SetLength(keyBtypts, 17);
+  Move(org[1], keyBtypts[0], 16);
+
+  salt := BCrypt.BsdBase64Encode(keyBtypts, Length(keyBtypts));
+  SetLength(salt, 22);
+  
+  output := BCrypt.HashPassword(key, salt);
+
+  Edit_output_1.Text := output;
+  Edit_salt.Text := salt;
+
+  Label_note_1.Caption := 'Bcrypt ok.';
+end;
+
+procedure TForm1.BitBtn_check_1Click(Sender: TObject);
+begin
+  //
+end;
+
+procedure TForm1.SpeedButton_upClick(Sender: TObject);
+begin
+  Edit_key.Text := Edit_key_1.Text;
+  Edit_output.Text := Edit_output_1.Text;
+  Label_tip.Caption := '';
+  BitBtn_check.Click;
+end;
+
 end.
+
